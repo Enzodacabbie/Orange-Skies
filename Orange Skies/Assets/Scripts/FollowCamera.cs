457eeb88afc04db4deca1fad9542d82621d74087
VictorTPhan <71913548+VictorTPhan@users.noreply.github.com>
2020-10-02 18:32:01 -0700

    Air Dash, Multiple Jumps, and Gradual Acceleration
    -player can now air dash
    -can now configure multi-jumps
    -can now configure increase in speed over game time (set to 0 for now)
    
    some files got butchered when i switched to master (???) so if there are any issues please contact me.

---
+++ Orange Skies/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public GameObject player;
+
+    //done in lateupdate so that we can get the player's position correctly before moving camera
+    private void LateUpdate()
+    {
+        if (player != null)
+        {
+            transform.position = new Vector3(player.transform.position.x, 0, -10);
+        }
+    }
+}
---
+++ Orange Skies/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speedModifier;
+
+    [Header("Ground")]
+    public float speed;
+    bool facingRight;
+
+    [Header("Ground Check")]
+    public Transform groundCheckOrigin;
+    public float groundCheckDistance;
+    public LayerMask groundLayer; //input Ground layer here
+    private bool grounded;
+    private Vector2 groundCheckBoxSize;
+    public int jumpLimit = 1;
+    private int jumps;
+
+    [Header("Jumping")]
+    public float jumpHeight;
+    public float airSpeed;
+    private float GRAVITY; //never change
+
+    [Header("Dashing")]
+    public float dashSpeed;
+    public int dashLimit = 1;
+    public float dashDuration = 0.5f;
+    private int dashes;
+
+    Rigidbody2D rb;
+
+    //runs before game starts
+    private void Awake()
+    {
+        //grab a reference to the rigidbody before the game starts
+        rb = GetComponent<Rigidbody2D>();
+
+        //get the force of gravity from the physics menu
+        GRAVITY = Physics2D.gravity.y;
+
+        //let the ground check box size be 95% the width
+        //of the player's transform scale, with height
+        //groundcheckDistance.
+        //cache it here to save on memory
+        groundCheckBoxSize = transform.localScale;
+        groundCheckBoxSize.x *= 0.95f;
+        groundCheckBoxSize.y = groundCheckDistance;
+    }
+
+    private void FixedUpdate()
+    {
+        //grounded if ground detected beneath player
+        grounded = Physics2D.OverlapBox(groundCheckOrigin.position, groundCheckBoxSize, 0, groundLayer);
+    }
+
+    //runs every frame
+    private void Update()
+    {
+        speed += Time.deltaTime * speedModifier;
+        airSpeed += Time.deltaTime * speedModifier;
+
+        //resets jump and dash counter
+        if (grounded && rb.velocity.y <= 0)
+        {
+            jumps = jumpLimit;
+            dashes = dashLimit;
+        }
+        else
+        {
+            if (Input.GetKeyDown("z") && dashes > 0)
+            {
+                StartCoroutine(Dash());
+            }
+        }
+
+        //flip if player faces and moves in the same direction
+        //written here (kinda) inefficiently for readability
+        if (facingRight && rb.velocity.x > 0)
+            Flip();
+        else if (!facingRight && rb.velocity.x < 0)
+            Flip();
+
+        //moves player horizontally
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
+
+        //if player has jumps, you can jump
+        if (Input.GetKeyDown("up") && jumps > 0)
+        {
+            //if in midair and jump limit is 1, don't jump.
+            if (grounded || jumpLimit != 1)
+            {
+                //physics equation.
+                //upwards velocity = square root of (jump height * -2 * gravity)
+                rb.velocity = new Vector2(rb.velocity.x, Mathf.Sqrt(jumpHeight * -2f * GRAVITY));
+            }
+            
+            //if you fell off a ledge and you jump, do essentially a double-jump
+            if (!grounded && jumps == jumpLimit)
+            {
+                jumps--;
+            }
+
+            jumps--;
+        }
+    }
+
+    //flips player to the other side
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 flipped = transform.localScale;
+        flipped.x *= -1;
+        transform.localScale = flipped;
+    }
+
+    //debug for inspector
+    private void OnDrawGizmosSelected()
+    {
+        Vector2 gizmoBoxSize = transform.localScale;
+        gizmoBoxSize.x *= 0.95f;
+        gizmoBoxSize.y = groundCheckDistance;
+
+        Gizmos.DrawWireCube(groundCheckOrigin.position, gizmoBoxSize);
+    }
+
+    //ienumerators/coroutines are essentially looping functions that rely on time. 
+    //this is advanced unity stuff so if anyone needs finds this too confusing
+    //then contact me and i'll do my best to do an alternative.
+    IEnumerator Dash()
+    {
+        float dashTime = dashDuration;
+
+        //ternary operator. if facing right, dashDir is Vector2.right, otherwise, it's left.
+        Vector2 dashDir = facingRight ? Vector2.left : Vector2.right;
+
+        while (dashTime > 0)
+        {
+            //count down
+            dashTime -= Time.deltaTime;
+
+            //the part that dashes
+            rb.velocity = dashDir * dashSpeed;
+
+            //halfway through the player can cancel their dash for precision
+            if (dashTime <= dashDuration/2 && Input.GetAxisRaw("Horizontal") != 0)
+                break;
+
+            //yield return null tells the program to wait for
+            //the next frame
+            yield return null;
+        }
+
+        dashes--;
+    }
+}
