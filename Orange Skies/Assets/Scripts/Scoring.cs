158ecaff44977a5327f4cf10e800d92ddabf8343
SHyguymoll <71913506+SHyguymoll@users.noreply.github.com>
2020-10-06 12:54:49 -0700

    Started work on Score System, commited Master to Score System for now

---
+++ Orange Skies/Assets/Scripts/Scoring.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoring : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int Score, highScore;
+    Text Display;
+    void Start()
+    {
+        Score = 0;
+    } 
+       
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*Score++; //Will be expanded and complexified later*/
+        
+        Display.text = Score.ToString();
+        if (highScore < Score) highScore = Score;
+    }
+}
